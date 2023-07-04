273510bcb7dbe4a75707381ea9af5aeb940b5c51
KPVISHNUSAI <kpvishnusai1111@gmail.com>
2023-07-04 07:21:33 +0000

    stopped at token error in API 1

---
+++ app.js
@@ -0,0 +1,86 @@
+const express = require("express");
+const path = require("path");
+const { open } = require("sqlite");
+const sqlite3 = require("sqlite3");
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
+
+const dbPath = path.join(__dirname, "covid19IndiaPortal.db");
+const app = express();
+app.use(express.json());
+
+let db = null;
+
+const initializeDBAndServer = async () => {
+  try {
+    db = await open({ filename: dbPath, driver: sqlite3.Database });
+    app.listen(3000, () => {
+      console.log("Server is running at http://localhost:3000/");
+    });
+  } catch (e) {
+    console.log(`DB Error: ${e.message}`);
+    process.exit(1);
+  }
+};
+initializeDBAndServer();
+
+const authenticateToken = (request, response, next) => {
+  let jwtToken;
+  const authHeader = request.headers["authorization"];
+  if (authHeader !== undefined) {
+    jwtToken = authHeader.split(" ")[1];
+  }
+  if (jwtToken === undefined) {
+    response.status(401).send("Invalid JWT Token");
+  } else {
+    jwt.verify(jwtToken, "MY_SECRET_TOKEN", async (error, payload) => {
+      if (error) {
+        response.send("Invalid JWT Token");
+      } else {
+        request.username = payload.username;
+        next();
+      }
+    });
+  }
+};
+
+//GET users API
+app.get("/", async (request, response) => {
+  const getUsersQuery = `SELECT * FROM user`;
+  const usersDetails = await db.all(getUsersQuery);
+  response.send(usersDetails);
+});
+
+//API 1 login
+app.post("/login/", async (request, response) => {
+  const { username, password } = request.body;
+  const selectUserQuery = `SELECT * FROM user WHERE username = '${username}'`;
+  const dbUser = await db.get(selectUserQuery);
+  if (dbUser === undefined) {
+    response.status(400).send("Invalid user");
+  } else {
+    const isPasswordMatched = await bcrypt.compare(password, dbUser.password);
+    if (isPasswordMatched) {
+      const payload = { username: username };
+      const jwtToken = jwt.sign(payload, "MY_SECRET_TOKEN");
+      response.send({ jwtToken });
+    } else {
+      response.status(400).send("Invalid Password");
+    }
+  }
+});
+
+//API 2
+//Returns a list of all states in the state table
+app.get("/states/", authenticateToken, async (request, response) => {
+  const getAllStatesQuery = `
+    SELECT 
+        state_id as stateId,
+        state_name as stateName,
+        population as population
+    FROM state;`;
+  const stateDetails = await db.all(getAllStatesQuery);
+  response.send(stateDetails);
+});
+
+module.exports = app;
